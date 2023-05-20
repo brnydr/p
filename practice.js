0fd50ddcefe26ceaaa5fb2a2cb20a8550b6178ad
Brian Yoder <brianyoder@gmail.com>
2023-05-19 19:10:49 -0700

    initial commit

---
+++ practice.js
@@ -0,0 +1,8 @@
+let myFirstVariable = 1 + 2; 
+
+const secondVariable = 2 + 5; 
+
+function addEmphasis(stringParam) {
+    const result = stringParam.toUpperCase().concat("!!!");
+    return result;
+}
